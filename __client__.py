abe0a78bf28b14d23f4da2d905d290dbc66411c5
Le Huy <huykingsofm@gmail.com>
2020-12-09 03:05:12 +0700

    Delete old files

--- __client__.py
+++
@@ -1,6 +0,0 @@
-from Client import Client
-
-if __name__ == "__main__":
-    client = Client(("192.168.137.1", 9999), verbosities= ("warning", "error", "notification"))
-    client.start()
-    pass
